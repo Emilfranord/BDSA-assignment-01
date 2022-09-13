7f8e9b7799b506d3f73873dd41187a135eb58a09
Frederik-Hoegh-Christensen <frederik.hoegh@gmail.com>
2022-09-13 14:31:32 +0200

    added implementation of method SplitLine

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,26 @@
 namespace Assignment1;
-
+using System.Text.RegularExpressions;
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    private static List<string> toWord(string str)
+{   
+    return Regex.Split(str, @"[^a-zA-Z0-9$]+").ToList();
+}
+    
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
+        foreach (var item in lines)
+        {
+            foreach (var line in toWord(item))
+            {
+                yield return line;
+            }
+        }
+    }
+        
+    
+
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) => throw new NotImplementedException();
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
