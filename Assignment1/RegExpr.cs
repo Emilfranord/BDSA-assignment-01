3a457f6b3381fd7342d57e6ef9e2038b5606bb35
Benjamin Edon Hagelin <bhag@itu.dk>
2022-09-13 16:24:10 +0200

    Add method for RegExResolutions and tests

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -20,7 +20,22 @@ public static class RegExpr
         
     
 
-    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions) {
+
+    var pattern = "(?<width>[0-9]+)x(?<height>[0-9]+)";
+    var resolutionsChanged = RegExpr.SplitLine(resolutions);
+    foreach (var reso in resolutionsChanged)
+    {
+        var temp = Regex.Matches(reso, pattern);
+        foreach (Match match in temp)
+        {
+            var width = Int32.Parse(match.Groups["width"].Value);
+            var height = Int32.Parse(match.Groups["height"].Value);
+            yield return (width, height);
+        }
+    }
+
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
